fbc1697f8480008856978be870a2b30b5b8e8a69
liran <liranc@wobi.co.il>
2017-04-26 10:44:33 +0300

    fixed the package build

--- demo/components/app.js
+++ demo/components/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import FacebookReduxLogin from '../../src/facebook-login.jsx';
-import FacebookReduxLogin from '../../src/facebook-login.jsx';
+import FacebookReduxLogin from '../../build/index.js';
 
 export default class App extends Component {
   render() {
